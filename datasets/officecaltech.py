d1e606d0209ec6db9db71c540c71cd607c4f5464
smart0eddie <smart0eddie@gmail.com>
2024-07-16 22:28:29 +0800

    syntax error

--- datasets/officecaltech.py
+++ datasets/officecaltech.py
@@ -71,7 +71,7 @@ class FedLeaOfficeCaltech(FederatedDataset):
 
     def get_data_loaders(self, selected_domain_list):
 
-        using_list = self.DOMAINS_LIST if selected_domain_list == [] else selected_domain_list
+        using_list = self.DOMAINS_LIST if len(selected_domain_list) == 0 else selected_domain_list
 
         nor_transform = self.Nor_TRANSFORM
         train_dataset_list = []
